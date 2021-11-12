15f2822a31110b1ff00b2f99f01b261811f6307c
dprashin <dprashin@gmail.com>
2021-11-12 13:51:34 +0100

    Seed database

--- API/Program.cs
+++ API/Program.cs
@@ -14,7 +14,7 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
 
@@ -24,7 +24,9 @@ namespace API
             try
             {
                 var context = services.GetRequiredService<DataContext>();
-                context.Database.Migrate();
+                await context.Database.MigrateAsync();
+                //seed data
+                await Seed.SeedData(context);
             }
             catch (Exception ex)
             {
@@ -33,7 +35,7 @@ namespace API
                 logger.LogError(ex, "An error occured during migration");
             }
 
-            host.Run();
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
