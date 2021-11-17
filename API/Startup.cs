56d568f132bce71b27e3f7a302939d8b3b899416
dprashin <dprashin@gmail.com>
2021-11-17 19:00:22 +0100

    Add CORS policy

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,9 +33,16 @@ namespace API
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
             services.AddDbContext<DataContext>(opt =>
-           {
-               opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-           });
+            {
+                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +58,8 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
